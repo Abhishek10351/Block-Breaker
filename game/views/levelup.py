54b8a07330be64dad25f3936cb8f3098703de7f1
Abhishek Kashyap <abhik5567@gmail.com>
2024-10-12 14:13:08 +0530

    improve levelup screen

--- game/views/levelup.py
+++ game/views/levelup.py
@@ -2,7 +2,7 @@ import arcade
 import arcade.gui
 from constants import *
 import styles
-
+from widgets import IconButton
 from layout import StarLayout
 
 
@@ -11,16 +11,46 @@ class LevelUpView(arcade.View):
 
     def __init__(self):
         self.gui_manager = arcade.gui.UIManager()
+        self.icons_path = UI_PATH / "icons"
+
+        self.icons = [
+            "next.png",
+            "play.png",
+            "levels.png",
+        ]
+        self.row_layout = arcade.gui.UIBoxLayout(vertical=True, space_between=50)
+
+        self.button_layout = arcade.gui.UIBoxLayout(vertical=False, space_between=10)
+        self.buttons = [IconButton(i, i) for i in self.icons]
+        for i, j in enumerate(self.buttons):
+            if i == 0:
+                j.on_click = self.next
+            elif i == 1:
+                j.on_click = self.play
+            elif i == 2:
+                j.on_click = self.levels
+
+        for i in self.buttons:
+            self.button_layout.add(i)
+
+        self.highscore_label = arcade.gui.UILabel(
+            f"Highscore: 1000", font_size=24, font_name="Kenney Future"
+        )
 
         self.stars = 3
-
         self.star_layout = StarLayout(self.stars)
-        self.star_anchor_layout = arcade.gui.UIAnchorLayout(
-            anchor_x="center", anchor_y="center"
-        )
-        self.star_anchor_layout.add(self.star_layout)
+        # self.star_layout.with_background(
+        #     color=arcade.color.Color(237, 119, 31, 200),
+        # )
+        self.anchor_layout = arcade.gui.UIAnchorLayout()
 
-        self.gui_manager.add(self.star_anchor_layout)
+        self.row_layout.add(self.star_layout)
+        self.row_layout.add(self.highscore_label)
+        self.row_layout.add(self.button_layout)
+
+        self.anchor_layout.add(self.row_layout, anchor_y="top", align_y=-200)
+
+        self.gui_manager.add(self.anchor_layout)
 
         super().__init__()
 
@@ -30,27 +60,32 @@ class LevelUpView(arcade.View):
             f"You have completed level {self.window.current_level}.\nClick to continue."
         )
 
-    @property
-    def text(self):
-        return arcade.Text(
-            self.message,
-            300,
-            400,
-            bold=True,
-            width=500,
-            anchor_x="center",
-            font_name="Kenney Future",
-            font_size=40,
-            multiline=True,
-        )
-
     def on_draw(self):
         """Draw this view"""
         self.clear()
-        self.text.draw()
         self.gui_manager.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
         """If the user presses the mouse button, re-start the game."""
         game_view = self.window.views["Menu"]
         self.window.show_view(game_view)
+
+    def on_show_view(self):
+        self.gui_manager.enable()
+
+    def on_hide_view(self):
+        self.gui_manager.disable()
+
+    def play(self, event):
+        self.window.show_view(self.window.views["Level"])
+
+    def next(self, event):
+        total_levels = self.window.total_levels
+        if self.window.current_level < total_levels:
+            self.window.current_level += 1
+            self.window.show_view(self.window.views["Level"])
+        else:
+            self.window.show_view(self.window.views["Menu"])
+
+    def levels(self, event):
+        self.window.show_view(self.window.views["LevelSelection"])
